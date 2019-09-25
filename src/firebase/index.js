6d39f716ca6c050bec420800e00b8e9b30a3c038
Ethan Telila <ethan11675@gmail.com>
2019-09-24 23:41:15 -0400

    add vue web app for meals

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+  
+  const config = {
+    apiKey: "AIzaSyDwfF7x2ihQx43eTGgl_iZ9MoHDNC07Oj4",
+    authDomain: "meal-prep-43471.firebaseapp.com",
+    databaseURL: "https://meal-prep-43471.firebaseio.com",
+    projectId: "meal-prep-43471",
+    storageBucket: "meal-prep-43471.appspot.com",
+    messagingSenderId: "811000611966"
+  };
+  firebase.initializeApp(config);
